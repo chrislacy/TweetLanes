2b21a5f29c61823948f44cb86cb492e6aa888898
Jason Famularo <jason@famularo.org>
2013-05-29 23:41:46 -0400

    Breaking build so you have to provide your own API keys.

--- android/client/src/com/tweetlanes/android/ConsumerKeyConstants.java
+++ android/client/src/com/tweetlanes/android/ConsumerKeyConstants.java
@@ -8,9 +8,11 @@ package com.tweetlanes.android;
  * To change this template use File | Settings | File Templates.
  */
 public class ConsumerKeyConstants {
-    // Create a new key/secret here: https://dev.twitter.com/apps
-    public static final String TWITTER_CONSUMER_KEY = "Put your key here";
-    public static final String TWITTER_CONSUMER_SECRET = "Put your secret here";
-    public static final String APPDOTNET_CONSUMER_KEY = "Put your key here";
-    public static final String APPDOTNET_CONSUMER_SECRET = "Put your secret here";
+    // Create a new key/secret here: https://dev.twitter.com/apps and here: https://account.app.net/developer/app/create/
+    // Update below, uncomment lines:
+
+    //public static final String TWITTER_CONSUMER_KEY = "Put your key here";
+    //public static final String TWITTER_CONSUMER_SECRET = "Put your secret here";
+    //public static final String APPDOTNET_CONSUMER_KEY = "Put your key here";
+    //public static final String APPDOTNET_CONSUMER_SECRET = "Put your secret here";
 }
